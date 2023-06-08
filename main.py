953cf50ae71c3eaf5b1719218113c7b3820da04a
egor-antipsychotic <m.egor.antipsychotic@gmail.com>
2023-06-08 14:57:44 +0300

    add LC for output; replace saved 'total' with output calculation

--- main.py
+++ main.py
@@ -9,36 +9,37 @@ def data_transform(datafile):
     import re
     timesheet = {}
     for i in range(datafile.__len__()):
-        name = ''.join(re.findall(r'\w*\D', datafile[i]))[0:-1]  # [0:-1] выполняет исключительно косметическую роль
+        name = ''.join(re.findall(r'\w*\D', datafile[i]))[:-1]
         hours = re.findall(r'\d+$', datafile[i])
         if name not in timesheet:
-            timesheet[name] = {
-                'hours': hours[0],
-                'total': int(hours[0])
-            }
+            timesheet[name] = hours
         else:
-            timesheet[name]['hours'] += f', {hours[0]}'
-            timesheet[name]['total'] += int(hours[0])
+            timesheet[name] += hours
     return timesheet
 
 
 def output_all(mod_data):
-    for name in mod_data.items():
-        print(f"{name[0]}: {name[1]['hours']}; sum: {name[1]['total']}")
+    final_data = ''
+    for name, hours in mod_data.items():
+        lc = [int(i) for i in hours]
+        final_data += f"{name}: {str(lc)[1:-1]}; sum: {sum(lc)}\n"
+    return final_data
 
 
-def output_one(mod_data, *names):
+def output_some(mod_data, *names):
+    final_data = ''
     for name in names:
-        print(f"{name}: {mod_data[name]['hours']}; sum: {mod_data[name]['total']}")
+        lc = [int(i) for i in mod_data[name]]
+        final_data += f"{name}: {str(lc)[1:-1]}; sum: {sum(lc)}\n"
+    return final_data
 
 
 if __name__ == '__main__':
-
     # формирование словаря с данными
     transformed_data = data_transform(data_input())
 
     # вывод статистики по всем работникам
-    output_all(transformed_data)
+    print(output_all(transformed_data))
 
     # опциональный вывод статистики по работнику/работникам
-    # output_one(transformed_data, 'Василий', 'Андрей1', 'Андрей 1')
+    print(output_some(transformed_data, 'Василий'))
